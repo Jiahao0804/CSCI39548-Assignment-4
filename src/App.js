bb5606e6a6a1268fa3ff099e3470d65ab4d9494f
Jiahao <93109917+Jiahao0804@users.noreply.github.com>
2023-04-29 02:11:25 -0400

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
   constructor() {  // Create and initialize state
     super(); 
     this.state = {
-      accountBalance: 1234567.89,
+      accountBalance: 0,  // set the account balance to 0
       creditList: [],
       debitList: [],
       currentUser: {
@@ -35,6 +35,55 @@ class App extends Component {
     this.setState({currentUser: newUser})
   }
 
+  // Update creditList
+  addCredit = (e) => {
+    this.setState({creditList: this.state.creditList([{
+      amount: e.amount,
+      description: e.description,
+      date: e.date,
+    }])});
+
+    // update the account balance and set the decimal in 2 place
+    this.setState({accountBalance: (float(this.state.accountBalance) + float(e.amount)).toFixed(2)})
+  }
+
+  // Update debitList
+  addDebit = (e) => {
+    this.setState({debitList: this.state.debitList([{
+      amount: e.amount,
+      description: e.description,
+      date: e.date,
+    }])});
+
+    // update the account balance and set the decimal in 2 place
+    this.setState({accountBalance: (float(this.state.accountBalance) - float(e.amount)).toFixed(2)})
+  }
+
+  // get data from API
+  async componentDidMount()
+  {
+    let credits = await axios.get("https://johnnylaicode.github.io/api/credits.json");
+    let debits = await axios.get("https://johnnylaicode.github.io/api/debits.json");
+
+    credits = credits.data;
+    debits = debits.data;
+
+    let total_credits = 0;
+    let total_debits = 0;
+
+    credits.forEach((credit) => {
+      total_credits = total_credits + credit.amount;
+    });
+
+    debits.forEach((debits) => {
+      total_debits = total_debits + debits.amount;
+    });
+
+    let Update_account_balance = (total_credits - total_debits).toFixed(2);
+
+    this.setState({accountBalance: Update_account_balance});
+  }
+
   // Create Routes and React elements to be rendered using React components
   render() {  
     // Create React elements and pass input props to components
@@ -43,8 +92,8 @@ class App extends Component {
       <UserProfile userName={this.state.currentUser.userName} memberSince={this.state.currentUser.memberSince} />
     )
     const LogInComponent = () => (<LogIn user={this.state.currentUser} mockLogIn={this.mockLogIn} />)
-    const CreditsComponent = () => (<Credits credits={this.state.creditList} />) 
-    const DebitsComponent = () => (<Debits debits={this.state.debitList} />) 
+    const CreditsComponent = () => (<Credits credits={this.state.creditList} addCredit={this.addCredit} accountBalance={this.state.accountBalance}/>) 
+    const DebitsComponent = () => (<Debits debits={this.state.debitList} addDebit={this.addDebit} accountBalance={this.state.accountBalance}/>) 
 
     // Important: Include the "basename" in Router, which is needed for deploying the React app to GitHub Pages
     return (
